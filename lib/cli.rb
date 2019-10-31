8709f3e2eae3fa383394224fb1a17ae566d16a75
Victoria Hale <vic.hale74@gmail.com>
2019-10-31 00:38:59 +0000

    modified scraper

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,10 +5,12 @@ require 'pry'
 def run 
   puts "Today's Top Stories:"
   puts " "
-  
+  binding.pry
   doc = Nokogiri::HTML(open("https://www.huffpost.com"))
-  doc.css("div.zone__content")[1].css("card card--left card--media-left card--standard js-card yr-card").each do |art|
-    article = Articles.new
-    article.title = art.css("div.card__details").css("div.card__headlines").css("card__headline").css("div.card__headline__text").text
+  doc.css("div.zone__content")[1].css("card__content").each do |art|
+    
+    # article = Articles.new
+    # article.title = art.css("div.card__details").css("div.card__headline__text").text
+    
   end
 end 
