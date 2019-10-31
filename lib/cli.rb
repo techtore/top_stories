14796c79d8b788f021c0e13d75ea2285f6e40a1f
Victoria Hale <vic.hale74@gmail.com>
2019-10-31 00:27:01 +0000

    scrape

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,9 @@ def run
   puts "Today's Top Stories:"
   puts " "
   
-  doc = Nokogiri::HTML(open(https://www.huffpost.com/))
-  
+  doc = Nokogiri::HTML(open("https://www.huffpost.com"))
+  doc.css("div.zone__content")[1].css("card card--left card--media-left card--standard js-card yr-card").each do |art|
+    article = Articles.new
+    article.title = art.css("div.card__details").css("div.card__headlines").css("card__headline").css("div.card__headline__text").text
+  end
 end 
