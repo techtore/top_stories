24ba5833e1822161d4e8eeb79e0586aab1f37a15
Victoria Hale <vic.hale74@gmail.com>
2019-10-31 04:26:34 +0000

    modified scraper

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Scraper
   
   def self.scrape_articles
@@ -6,6 +7,7 @@ class Scraper
     
         article = Article.new
         article.title = art.css("div.card__details").css("div.card__headline__text").text.strip
+        article.url = art.css("a").attribute("href").value
     end
   end
   
--- lib/cli.rb
+++ lib/cli.rb
@@ -5,13 +5,15 @@ class CLI
   def run 
     puts "Today's Top Stories:"
     puts " "
+    Scraper.scrape_articles
+    binding.pry
     print_title
     menu
   end
   
   def print_article
-    Article.all.each.with_index(1) do |title, index|
-      puts "#{index}. #{title}"
+    Article.all.each.with_index(1) do |article, index|
+      puts "#{index}. #{article.title}"
     end
   end
   
